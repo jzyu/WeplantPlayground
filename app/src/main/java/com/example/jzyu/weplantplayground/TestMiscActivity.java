8f251bedabfe7cb5e0b0a70cac34f4ddb341f7ce
jzyu <971118309@qq.com>
2017-04-13 10:13:21 +0800

    add test of ImageView animation

--- app/src/main/java/com/example/jzyu/weplantplayground/TestMiscActivity.java
+++ app/src/main/java/com/example/jzyu/weplantplayground/TestMiscActivity.java
@@ -1,13 +1,31 @@
 package com.example.jzyu.weplantplayground;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class TestMiscActivity extends AppCompatActivity {
+
+    @Bind(R.id.iv_status)
+    ImageView ivStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_misc);
+        ButterKnife.bind(this);
+
+        initView();
+    }
+
+    private void initView() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.progressbar_circle);
+        ivStatus.setImageResource(R.drawable.loading);
+        ivStatus.startAnimation(anim);
     }
 }
